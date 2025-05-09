60abbfdbbb75ed89053f7ab0911d48af3df0734a
robonexx <robertwagar@gmail.com>
2025-05-09 10:23:10 +0200

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: { unoptimized: true },
+  reactStrictMode: true,
+  webpack(config) {
+    // Add rule for handling SVG files
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
