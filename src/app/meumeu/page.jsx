926629ccc6e5052726cf6db056c20b673b0d3eb6
robonexx <robertwagar@gmail.com>
2025-05-10 17:06:49 +0200

    ...

--- src/app/meumeu/page.jsx
+++ src/app/meumeu/page.jsx
@@ -4,17 +4,14 @@ import './meumeu.scss';
 import Body1 from '@/components/Body1';
 import Body2 from '@/components/Body2';
 
-
 const Meumeu = () => {
   return (
     <div className='meumeu'>
       <section>
-        <Body1 />
-      </section>
-
-      <section>
         <Body2 />
-      
+      </section>
+      <section>
+        <Body1 />
       </section>
     </div>
   );
